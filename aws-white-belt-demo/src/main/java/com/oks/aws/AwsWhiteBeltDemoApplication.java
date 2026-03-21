ff9167ac17485a91309ccd813c3637b72b115b9f
Omkar Singh <omkarsinghaws@gmail.com>
2026-03-21 23:25:42 +0530

    Base Code

---
+++ aws-white-belt-demo/src/main/java/com/oks/aws/AwsWhiteBeltDemoApplication.java
@@ -0,0 +1,13 @@
+package com.oks.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsWhiteBeltDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsWhiteBeltDemoApplication.class, args);
+	}
+
+}
---
+++ aws-white-belt-demo/src/test/java/com/oks/aws/AwsWhiteBeltDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oks.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsWhiteBeltDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
